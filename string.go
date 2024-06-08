15e5e136ccab5093edb803e3610cf5d40f9b2362
vlaship <vlaship@outlook.com>
2024-06-08 15:14:11 -0400

    added runes, bytes

--- string.go
+++ string.go
@@ -6,9 +6,29 @@ const (
 	cut        = 2
 )
 
+// String masks the middle part of a string.
 func String(s string) string {
 	if len(s) <= minLen {
 		return maskedPart
 	}
 	return s[:cut] + maskedPart + s[len(s)-cut:]
+}
+
+// Strings masks the middle part of a string slice.
+func Strings(s []string) []string {
+	for i, v := range s {
+		s[i] = String(v)
+	}
+
+	return s
+}
+
+// Runes masks the middle part of a rune slice.
+func Runes(r []rune) []rune {
+	return []rune(String(string(r)))
+}
+
+// Bytes masks the middle part of a byte slice.
+func Bytes(b []byte) []byte {
+	return []byte(String(string(b)))
 }
--- string_test.go
+++ string_test.go
@@ -54,3 +54,24 @@ func TestMaskLongStringWithSpecialCharacters(t *testing.T) {
 	expected := "ab" + maskedPart + "xz"
 	assert.Equal(t, expected, result, "String should mask the middle part of a long input with special characters")
 }
+
+func TestMaskStrings(t *testing.T) {
+	input := []string{"123", "12345abcde", "12345", "123456", "", "*****", "$%#", "ab" + strings.Repeat("$%#", 50) + "xz"}
+	result := Strings(input)
+	expected := []string{maskedPart, "12" + maskedPart + "de", maskedPart, "12" + maskedPart + "56", maskedPart, maskedPart, maskedPart, "ab" + maskedPart + "xz"}
+	assert.Equal(t, expected, result, "Strings should mask the middle part of each string in the slice")
+}
+
+func TestMaskRunes(t *testing.T) {
+	input := []rune("1234567890")
+	result := Runes(input)
+	expected := []rune("12" + maskedPart + "90")
+	assert.Equal(t, expected, result, "Runes should mask the middle part of a rune slice")
+}
+
+func TestMaskBytes(t *testing.T) {
+	input := []byte("1234567890")
+	result := Bytes(input)
+	expected := []byte("12" + maskedPart + "90")
+	assert.Equal(t, expected, result, "Bytes should mask the middle part of a byte slice")
+}
